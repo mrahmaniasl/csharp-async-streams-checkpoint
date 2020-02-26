7cec666cc4e1767d3478f44b20ebfc8ab65d5b87
mrahmaniasl <mohammad.asl@autodesk.com>
2020-02-26 08:47:06 -0800

    changed to async

---
+++ CsharpAsyncStreamCheckpoint/Test.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpAsyncStreamsCheckpoint
+{
+    [TestClass]
+    public class TestsBefore
+    {
+
+        [TestMethod]
+        public async Task Test1()
+        {
+            string result = "";
+            await foreach (var student in GetStudentsAsync())
+            {
+                result = result + ($"{student.FirstName} {student.LastName} - ");
+            }
+
+            Assert.AreEqual("John Doe - Jane Doe - John Smith - ", result);
+        }
+
+        static async IAsyncEnumerable<Student> GetStudentsAsync()
+        {
+            await Task.Delay(2000);
+
+            yield return new Student() { FirstName = "John", LastName = "Doe", Email = "john.doe@gmail.com", Grade = 'A' };
+            yield return new Student() { FirstName = "Jane", LastName = "Doe", Email = "jane.doe@galvanize.com", Grade = 'B' };
+            yield return new Student() { FirstName = "John", LastName = "Smith", Email = "john.smith@galvanize.com", Grade = 'C' };
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public char Grade { get; set; }
+    }
+}
